86ff73ff1306483dd4451e1b6c26766ea9be43fa
Rykitik <90599517+Rykitik@users.noreply.github.com>
2022-02-10 12:45:16 +0300

    Фиксы

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template lang ="pug">
 #app.h-100
   MainPage
+  Footer
+  Header
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
 import MainPage from "@/views/MainPage.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "App",
   components: {
     MainPage,
+    Footer,
+    Header,
   },
 };
 </script>
