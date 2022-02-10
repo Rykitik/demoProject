3c7115a1ab720c1d2daee4391fbbaeace6d54233
Rykitik <90599517+Rykitik@users.noreply.github.com>
2022-02-10 12:38:48 +0300

    Шаблон страницы

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,3 +1,48 @@
-<template lang ="pug">
-#mainPage Hello
+<template>
+  <div class="d-flex flex-column h-100">
+    <header>
+      <!-- Fixed navbar -->
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Design.pro</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </nav>
+    </header>
+
+    <!-- Begin page content -->
+    <main role="main" class="flex-shrink-0">
+      <div class="container">
+        <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+        <p class="lead">
+          Pin a footer to the bottom of the viewport in desktop browsers with
+          this custom HTML and CSS. A fixed navbar has been added with
+          <code>padding-top: 60px;</code> on the
+          <code>main &gt; .container</code>.
+        </p>
+        <p>
+          Back to
+          <a href="/docs/4.3/examples/sticky-footer/"
+            >the default sticky footer</a
+          >
+          minus the navbar.
+        </p>
+      </div>
+    </main>
+
+    <footer class="footer mt-auto py-3">
+      <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
+  </div>
 </template>
+<script>
